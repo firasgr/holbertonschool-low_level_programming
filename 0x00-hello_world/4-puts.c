5072ea92722cbfafac18497d846b64c99801db55
firasgr <firasgrati908@yahoo.com>
2020-06-10 16:19:56 +0200

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- *print string with return value of zero
- */
+ *main - print string to the console with a newline
+ *Return: string with return value of zero
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
