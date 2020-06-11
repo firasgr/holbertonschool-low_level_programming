ad82a8e1fd1f4099aa442e0baaa20939902a1e31
firasgr <firasgrati908@yahoo.com>
2020-06-11 13:59:22 +0200

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *Return: return value zero (success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
