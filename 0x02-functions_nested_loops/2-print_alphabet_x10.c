566768be0a604a0a5badfa779a2b8f94d93a8346
firasgr <59062737+firasgr@users.noreply.github.com>
2020-06-16 10:13:34 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@ void print_alphabet_x10(void)
 {
 char alpha;
 int count;
-for (int count = '0'; count <= '9'; count++)
+for (count = '0'; count <= '9'; count++)
 {
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
