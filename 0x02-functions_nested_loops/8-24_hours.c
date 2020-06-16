f0ac615bf48cc37e42df923cb7d0c7640d20356f
firasgr <firasgrati908@yahoo.com>
2020-06-16 14:25:21 +0200

    every hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *jack_bauer -  a function that prints every minute of the day of Jack Bauer
+ * @n: int
+ *Return: 0 or 1
+ */
+void jack_bauer(void)
+{
+int a, b;
+for (a = 0 ; a < 24 ; a++)
+{
+for (b = 0 ; b < 60 ; b++)
+{
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
+_putchar(':');
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+_putchar('\n');
+}
+}
+return (0);
+}
