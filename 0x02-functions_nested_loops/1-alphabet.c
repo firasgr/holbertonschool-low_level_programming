55b8ecf45d85caa55df95adbe6bddd8feeeb015e
firasgr <59062737+firasgr@users.noreply.github.com>
2020-06-16 09:36:43 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *main - main function
- *Return: return value zero (success)
-*/
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 char alpha = 'a';
