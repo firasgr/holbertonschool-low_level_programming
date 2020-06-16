e23322a7e3662b900a2cb09f998356eb11bdf9e3
firasgr <firasgrati908@yahoo.com>
2020-06-16 09:57:21 +0200

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- *print_alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
- */
+ *print_alphabet - a function that prints the alphabet in lowercase .
+*/
 void print_alphabet(void)
 {
 char alpha = 'a';
