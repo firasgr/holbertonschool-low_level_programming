e452e92c80233602f6cd0674bc1f9e6e56c48f2f
firasgr <firasgrati908@yahoo.com>
2020-06-16 12:05:16 +0200

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,18 @@
 #include "holberton.h"
 /**
- *main - main function
+ *_abs - a function that computes the absolute value of an integer.
+ *@n: int
  *Return: return value zero (success)
 */
 int _abs(int)
 {
-
+int n;
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
 }
