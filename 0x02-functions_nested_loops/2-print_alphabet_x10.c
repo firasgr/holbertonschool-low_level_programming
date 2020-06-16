9f7f0ae445c593322751290268b126e2235d1794
firasgr <59062737+firasgr@users.noreply.github.com>
2020-06-16 09:51:42 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
- *main - main function
- *Return: return value zero (success)
+ *print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase, followed by a new line.
 */
 void print_alphabet_x10(void)
 {
