4c306a54d38c1c9bf64c12945bf97668dc1e2c32
firasgr <59062737+firasgr@users.noreply.github.com>
2020-06-17 22:53:02 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,20 @@
  */
 void times_table(void)
 {
-int a, b, c;
-c = a * b;
+int a, b;
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
 {
-if (c > 9)
+if (a * b > 9)
 {
 if (b != 0)
 {
 _putchar(',');
 _putchar(' ');
 }
-_putchar(c / 10 + '0');
-_putchar(c % 10 + '0');
+_putchar(a * b / 10 + '0');
+_putchar(a * b % 10 + '0');
 }
 else
 {
@@ -30,7 +29,7 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
-_putchar(c + '0');
+_putchar(a * b + '0');
 }
 }
 _putchar('\n');
