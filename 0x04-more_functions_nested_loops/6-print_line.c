37dd7c204278d236755e85dfd2de286d44dfed9b
firasgr <firasgrati908@yahoo.com>
2020-06-18 15:09:48 +0200

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_line - a function that print a line.
+ *@n: int
+ *Return: 0
+*/
+void print_line(int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+if(n > 0)
+{
+_putchar('_');
+}
+else
+{
+_putchar('\n');
+}
+}
+}
