3b65cd9107d5daefb3060c32e7b55b02acf41d79
firasgr <firasgrati908@yahoo.com>
2020-06-18 11:01:52 +0200

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isupper - check for uppercase character.
+ *@c: int
+ *Return: 0 or 1
+*/
+int _isupper(int c)
+{
+if (a >= 65 && a <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
