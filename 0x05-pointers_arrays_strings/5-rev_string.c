e8e310789d84025850507e512700420f91c8e61c
firasgr <firasgrati908@yahoo.com>
2020-06-23 21:59:22 +0200

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *rev_string - function that reverse a string
+ *@s: a string
+ */
+void rev_string(char *s)
+{
+char a;
+int b, c;
+b = 0;
+while (s[b + 1] != '\0')
+{
+b++;
+}
+c = b;
+b = 0;
+while (b < c / 2 + b)
+{
+a = s[b];
+s[b] = s[c - b];
+s[c - b] = a;
+b++;
+}
+}
