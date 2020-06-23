4ad28dcef9b87a7f52cd6b43ea79ca58644b7134
firasgr <firasgrati908@yahoo.com>
2020-06-23 21:46:53 +0200

    m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_array - prints an array
+ * @a: array
+ * @n: int
+*/
+void print_array(int *a, int n)
+{
+int b;
+for (b = 0; b < n; b++)
+{
+if (b != n - 1)
+{
+printf("%d, ", a[b]);
+}
+else
+{
+printf("%d", a[b]);
+}
+}
+printf("\n");
+}
