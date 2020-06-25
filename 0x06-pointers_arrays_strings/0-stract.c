8b0886bd53f431420b2b1889d8e76f044bb7e917
firasgr <59062737+firasgr@users.noreply.github.com>
2020-06-26 00:01:47 +0200

    Update 0-stract.c

--- 0x06-pointers_arrays_strings/0-stract.c
+++ 0x06-pointers_arrays_strings/0-stract.c
@@ -8,19 +8,5 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int a, b;
-a = 0;
-while (dest[a] != '\0')
-{
-a++;
-}
-dest[a] = src[0];
-b = 1;
-while (src[b] != '\0')
-{
-dest[a + b] = src[b];
-b++;
-}
-dest[a + b + 2] = '\0';
-return (dest);
+
 }
